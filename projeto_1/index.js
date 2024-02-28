398d16bed2f8090d8d91e38a95697c2199d5641f
AneGarcia <anelise300407@gmail.com>
2024-02-28 09:25:59 -0300

    Adicionei um novo formulário para calculadora, atualizeio index para receber dados

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -3,9 +3,14 @@ const app = express();
 const port = 3000
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res)=>{
     res.render('index')
+})
+
+app.get('/calculadora', (req, res) =>{
+    res.render('exerc_calculadora')
 })
 
 app.get('/Anelise', (req, res)=>{
@@ -23,6 +28,15 @@ app.get('/:nome/:sobrenome', (req, res)=>{
 app.get('/soma/:n1/:n2', (req, res)=>{
     const resultado = parseInt(req.params.n1)+parseInt(req.params.n2)
     res.send("O resultado da soma é " + resultado)
+})
+
+app.post('/pesquisar', (req,res) => {
+    res.send("Dados recebidos: "+ req.body.nome)
+})
+
+app.post('/calculadora', (req,res) =>{
+    const soma = parseInt(req.body.valor1)+parseInt(req.body.valor2)
+    res.send("O resultado da soma é " + soma)
 })
 
 app.listen(port)
